ad6f1bd61dd47a2bce5d2aa53f49705575efbac1
diogenesmedeiros <juniormedeiros20119@gmail.com>
2024-09-12 20:07:03 -0300

    Analytics vercel adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from "@vercel/analytics/react"
 
 import Dashboard from './components/Dashboard';
 import Notfound from './components/Notfound';
@@ -8,6 +9,7 @@ import Notfound from './components/Notfound';
 const App: React.FC = () => {
   return (
     <ChakraProvider>
+      <Analytics/>
       <Router>
         <Routes>
           <Route path='/' element={<Dashboard />} />
